e31a5451a2e0763899e580234e4e467ef713f940
Jesse Hitt <hittj77@gmail.com>
2022-02-21 21:23:35 -0600

    first commit

---
+++ codeWars.js
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
+// function multiply(a, b) {
+//     var c = a * b;
+//     console.log(c);
+// }
+// multiply(2, 3);
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+// function isEven(n) {
+//     return n % 2 == 0;
+// function isOdd(n) {
+//     return Math.abs(n % 2) == 1;
+// }}
+
+// function even_or_odd(number) {
+//     if (number % 2 == 0) {
+//         return "Even" }
+//     else {
+//     return "Odd" }
+
+// }
+// console.log(even_or_odd(2))
+
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+// function greet( name , owner ) {
+
+//     if (name == owner) {
+//         return "Hello boss"
+//     }
+//     else {
+//         return "Hello guest"
+//     }
+// }
+// console.log(greet("Derek", "Derek"))
